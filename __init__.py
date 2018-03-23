43b99e2238556b290abf8ed398afc79004e20680
Akash Malhotra <akash.jorss@gmail.com>
2018-03-23 12:20:59 +0800

    Image sending, resizing and receiving affirmation

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@ from scipy.misc import imread, imsave, imresize
 import matplotlib.pyplot as plot
 from urllib.request import Request, urlopen
 import numpy as np
-import io
-import cv2
+#import io
+#import cv2
 
 
 
@@ -13,14 +13,14 @@ import cv2
 def hello_world(request):
     return 'hello'
 
-@skygear.op('food:buy', user_required=False)
-def buy_food(food):
-    # TODO: call API about online shopping
-    # return an object to the SDK
-    return {
-        'success': True,
-        'food': food,
-    }
+#@skygear.op('food:buy', user_required=False)
+#def buy_food(food):
+#    # TODO: call API about online shopping
+#    # return an object to the SDK
+#    return {
+#        'success': True,
+#        'food': food,
+#    }
 
 
 @skygear.op('image:resize', user_required=False)
@@ -29,15 +29,19 @@ def resize_image(image_url, height = 125, width = 125):
 	#url_response = urlopen(image_url)
 	#img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
 	#file = io.StringIO(urlopen(image_url).read())
-	#img = imread(file)
-	resp = urlopen(image_url)
-	img = np.asarray(bytearray(resp.read()), dtype="uint8")
-	img = cv2.imdecode(img, -1)
-	print(img.shape)
-	modified_img = imresize(img, (height, width))
-	plot.imshow(img)
-	plot.show()
-	return modified_img
+    img = imread('cute-kittens.jpg')
+	#resp = urlopen(image_url)
+	#img = np.asarray(bytearray(resp.read()), dtype="uint8")
+	#img = cv2.imdecode(img, -1)
+    print(img.shape)
+    modified_img = imresize(img, (height, width))
+    print("Resizing was successful!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    return {
+           'success': True,
+        }
+	#plot.imshow(img)
+	#plot.show()
+	#return modified_img
 
 
 resize_image('http://www.petsworld.in/blog/wp-content/uploads/2014/09/cute-kittens.jpg')
