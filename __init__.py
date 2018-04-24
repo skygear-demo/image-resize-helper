d7de38de396bf02d90fd104f6b0a6859dbf0a503
akash <akash.jorss@gmail.com>
2018-04-24 17:21:23 +0800

    the resize function, not yet uploaded to skygear, still testing on local machine

--- __init__.py
+++ __init__.py
@@ -1,47 +1,80 @@
 import skygear
 from scipy.misc import imread, imsave, imresize
-import matplotlib.pyplot as plot
 from urllib.request import Request, urlopen
 import numpy as np
-#import io
-#import cv2
+from skygear.utils.assets import directory_assets
+from skygear.container import SkygearContainer
+from skygear.options import options # to obtain the master key
+import io
+from PIL import Image
+import requests
+
+
+master_container = SkygearContainer(
+    api_key='6d8edfb22a2a48d3b5c63c773dc46761'
+)
+
+def upload_image_to_skygear(bytes_data, filename, content_type):
+	host = 'https://imageprocessing.skygeario.com/'
+	asset_name = filename
+	api_key = '22aaa3c73b9249b6b838c0369ac06118'
+	headers = {
+        'Accept': 'application/json',
+        'Content-Type': content_type,
+        'X-Skygear-API-Key': api_key,
+	}
+	url = host + '/files/' + asset_name
+	response = requests.request(
+        'PUT',
+        url,
+        data=bytes_data,
+        headers=headers,
+	)
+	json_body = response.json()
+	print(json_body)
+	"""
+	output_name = json_body['result']['$name']
+	return output_name
+	"""
+
 
 
 
 # Visit https://<your-endpoint-url>/hello/ to view
+
+
+
 @skygear.handler('hello/')
 def hello_world(request):
     return 'hello'
 
-#@skygear.op('food:buy', user_required=False)
-#def buy_food(food):
-#    # TODO: call API about online shopping
-#    # return an object to the SDK
-#    return {
-#        'success': True,
-#        'food': food,
-#    }
 
 
 @skygear.op('image:resize', user_required=False)
-def resize_image(image_url, height = 125, width = 125):
-	#req = Request(image_url)
-	#url_response = urlopen(image_url)
-	#img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
-	#file = io.StringIO(urlopen(image_url).read())
-    img = imread('cute-kittens.jpg')
-	#resp = urlopen(image_url)
-	#img = np.asarray(bytearray(resp.read()), dtype="uint8")
-	#img = cv2.imdecode(img, -1)
-    print(img.shape)
-    modified_img = imresize(img, (height, width))
-    print("Resizing was successful!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    return {
+def resize_image(image_url="", height = 125, width = 125):
+    img = Image.open('cute-kittens.jpg')
+    global count
+
+    #print(img.shape)
+    output_image = img.resize((width,height))
+    print("Resizing was successful!")
+    #imsave('./temp/image'+str(count)+'.png', modified_img)
+    #count = count + 1;
+    memory_file = io.BytesIO()
+    output_image.save(memory_file, format='JPEG')
+
+    asset_name = upload_image_to_skygear(
+		memory_file.getvalue(),
+		'image.jpg',
+		'image/jpeg',
+		)
+    print(asset_name)
+    return asset_name
+
+"""
+	return {
            'success': True,
+           url:""
         }
-	#plot.imshow(img)
-	#plot.show()
-	#return modified_img
-
-
+"""
 resize_image('http://www.petsworld.in/blog/wp-content/uploads/2014/09/cute-kittens.jpg')
